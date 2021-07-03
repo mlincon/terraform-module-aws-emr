cb2571f968c76f96c6a8e9c4e75bec4e4bb302c4
mlincon <mehdi.lincon@gmail.com>
2021-07-03 14:42:12 +0200

    add s3 bucket

---
+++ s3.tf
@@ -0,0 +1,13 @@
+# s3
+
+# s3 bucket for storing logs
+resource "aws_s3_bucket" "s3-log-bucket" {
+    bucket = var.s3-log-bucket-name
+    region = var.region
+
+    versioning {
+      enabled = var.s3-log-bucket-versioning
+    }
+
+    tags = var.default_tags
+}
