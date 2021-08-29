1eca31053bc8b233b4e587fa86dcc1db0aa418d5
mlincon <mehdi.lincon@gmail.com>
2021-08-29 21:13:03 +0200

    add emr resources

---
+++ cluster/emr.tf
@@ -0,0 +1,25 @@
+resource "aws_emr_cluster" "cluster" {
+  name          = var.emr_name
+  release_label = var.emr_release_label
+  applications  = var.emr_applications
+
+  ec2_attributes {
+    subnet_id                         = aws_subnet.emr_subnet.id
+    emr_managed_master_security_group = aws_security_group.master_sg.id
+    emr_managed_slave_security_group  = aws_security_group.slave_sg.id
+    instance_profile                  = aws_iam_instance_profile.emr_profile.arn
+
+    key_name = aws_key_pair.emr_ssh_key.key_name
+  }
+
+  master_instance_group {
+    instance_type = var.emr_master_instance_type
+  }
+
+  core_instance_group {
+    instance_type  = var.emr_core_instance_type
+    instance_count = var.emr_core_instance_count
+  }
+
+  log_uri = "s3://${aws_s3_bucket.log_bucket.bucket}"
+}
--- variables.tf
+++ variables.tf
@@ -20,7 +20,7 @@ variable "default_tags" {
   type        = map(any)
   description = "Default tags in key-value pairs"
   default = {
-    Name : "terraform-custom-emr-module"
+    Name : "terraform_custom_emr_module"
   }
 }
 
@@ -33,76 +33,123 @@ variable "vpc_cidr_block" {
 variable "emr_role_name" {
   type        = string
   description = "The name of the EMR IAM role"
-  default     = "emr-role"
+  default     = "emr_role"
 }
 
+variable "emr_iam_instance_profile_name" {
+  type        = string
+  description = "The name of the EC2 instance profile"
+}
 
-variable "s3-log-bucket-name" {
+variable "s3_log_bucket_name" {
   type        = string
   description = "Name of the bucket to store cluster logs"
 }
 
-variable "s3-log-bucket-versioning" {
+variable "s3_log_bucket_versioning" {
   type        = string
   description = "Whether to activate versioninig"
   default     = "enabled"
 }
 
-variable "s3-bootstrap-bucket" {
+variable "s3_bootstrap_bucket" {
   type        = string
   description = "Bucket to store action/bootstrap scripts"
 }
 
-variable "s3-bootstrap-bucket-versioning" {
+variable "s3_bootstrap_bucket_versioning" {
   type        = string
   description = "Whether to activate versioninig"
   default     = "enabled"
 }
 
-variable "s3-input-bucket" {
+variable "s3_input_bucket" {
   type        = string
   description = "Bucket to store input files"
 }
 
-variable "s3-input-bucket-versioning" {
+variable "s3_input_bucket_versioning" {
   type        = string
   description = "Whether to activate versioninig"
   default     = "enabled"
 }
 
-variable "s3-output-bucket" {
+variable "s3_output_bucket" {
   type        = string
   description = "Bucket to store output"
 }
 
-variable "s3-output-bucket-versioning" {
+variable "s3_output_bucket_versioning" {
   type        = string
   description = "Whether to activate versioninig"
   default     = "enabled"
 }
 
-variable "ssh-key-name" {
+variable "emr_bootstrap_script" {
+  type        = string
+  description = "Name of the bootstrap script for EMR"
+  default     = "emr_bootstrap.sh"
+}
+
+
+variable "ssh_key_name" {
   type        = string
   description = "Name of the SSH key"
 }
 
-variable "ssh-key-path" {
+variable "ssh_key_path" {
   type        = string
   description = "Path and file name of the ssh key"
 }
 
-variable "sg-name-prefix" {
+variable "sg_name_prefix" {
   type        = string
   description = "Prefix for name of the EMR security groups"
 }
 
+# emr variables
+variable "emr_name" {
+  type        = string
+  description = "The name of the emr cluster"
+}
 
+variable "emr_release_label" {
+  type        = string
+  description = "The EMR version"
+  default     = "emr-5.33.0"
+}
 
+variable "emr_applications" {
+  type        = list(any)
+  description = <<EOT
+    List of the applications to install in the cluster. The default value is:
+    Spark 2.4.7 on Hadoop 2.10.1 YARN and Zeppelin 0.9.0
+  EOT
+  default     = ["Spark", "Zeppelin"]
+}
 
+variable "emr_master_instance_type" {
+  type        = string
+  description = <<EOT
+    The EC2 instance type for the master node. The master node manages the cluster
+    and typically runs master components of distributed applications, e.g. YARN.
+  EOT
+  default     = "m5.xlarge"
+}
 
+variable "emr_core_instance_type" {
+  type        = string
+  description = <<EOT
+    The EC2 instance type for the core nodes. The code nodes are managed by the 
+    master node. Code nodes coordinate data storage as part of the HDFS and perform
+    other parallel computation tasks on data.
+  EOT
+  default     = "m5.xlarge"
+}
 
-
-
-
-
+variable "emr_core_instance_count" {
+  type        = number
+  description = "The number of instances of core nodes"
+  default     = 2
+}
 
