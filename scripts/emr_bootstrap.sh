57184f3ee7a2d6486b96e92c36149134a13fad09
mlincon <mehdi.lincon@gmail.com>
2021-08-29 21:11:36 +0200

    store bootstrap script in bucket

---
+++ scripts/emr_bootstrap.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo pip install -U \
+    numpy \
+    matplotlib \
+    pandas
+    
--- storage/bootstap.tf
+++ storage/bootstap.tf
@@ -1,11 +1,24 @@
 # s3 bucket for bootstrap actions
-resource "aws_s3_bucket" "bootstrap-bucket" {
-  bucket = var.s3-bootstrap-bucket
+resource "aws_s3_bucket" "bootstrap_bucket" {
+  bucket = var.s3_bootstrap_bucket
   region = var.region
 
   versioning {
-    enabled = var.s3-bootstrap-bucket-versioning
+    enabled = var.s3_bootstrap_bucket_versioning
   }
+
+  tags = var.default_tags
+}
+
+# upload the bootstrap script in s3
+resource "aws_s3_bucket_object" "emr_bootstrap_script" {
+  depends_on = [
+    aws_s3_bucket.bootstrap_bucket
+  ]
+
+  bucket = aws_s3_bucket.bootstrap_bucket.id
+  key    = var.emr_bootstrap_script
+  source = "${path.module}/../scripts/${var.emr_bootstrap_script}"
 
   tags = var.default_tags
 }
